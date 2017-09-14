a9841529bd8c9fc96c629dd7381277ba0a5d2619
Bop <collin.cannavo@gmail.com>
2017-09-14 08:09:24 -0600

    organizing folders

---
+++ GradPlanTests/GradPlanTests.swift
@@ -0,0 +1,36 @@
+//
+//  GradPlanTests.swift
+//  GradPlanTests
+//
+//  Created by Collin Cannavo on 9/14/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import XCTest
+@testable import GradPlan
+
+class GradPlanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
