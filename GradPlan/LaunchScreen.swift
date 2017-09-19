b8212fc9f9e77d1b47f0d7d9c66be4f87e8c0606
Bop <collin.cannavo@gmail.com>
2017-09-18 18:16:01 -0600

    Adding Launch screen file

---
+++ GradPlan/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreen.swift
+//  GradPlan
+//
+//  Created by Collin Cannavo on 9/18/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LaunchScreen: UIViewController {
+    
+    
+    
+    
+}
